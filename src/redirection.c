9ba16cf81bc9ab01b2ee07fbab11e8fde5b58727
Théo Fabiano <theo.fabiano@epitech.eu>
2023-03-15 18:18:08 +0100

    ✏️ Fix typos

--- src/redirection.c
+++ src/redirection.c
@@ -7,27 +7,19 @@
 
 #include "mysh.h"
 
-void redirection_input(char **str, var_t *var, bool overwrite)
+void redirection_outpout(char **str, var_t *var, bool overwrite)
 {
     int oflag = O_CREAT | O_WRONLY;
     oflag = overwrite ? oflag | O_TRUNC : oflag | O_APPEND;
-    if (str[var->indice] && !my_strcmp(str[var->indice], "<")) {
-        if ((var->fd = open(str[var->indice + 1], O_RDONLY)) == -1) {
-            var->return_value = 1;
-            return;
-        }
-        dup2(var->fd, STDIN_FILENO);
-    } else {
-        if ((var->fd = open(str[var->indice + 1], oflag, 0644)) == -1) {
-            var->return_value = 1;
-            return;
-        }
-        dup2(var->fd, STDOUT_FILENO);
+    if ((var->fd = open(str[var->indice + 1], oflag, 0644)) == -1) {
+        var->return_value = 1;
+        return;
     }
+    dup2(var->fd, STDOUT_FILENO);
     close(var->fd);
 }
 
-void redirection_outpout(char **str, var_t *var)
+void redirection_input(char **str, var_t *var)
 {
     if ((var->fd = open(str[var->indice + 1], O_RDONLY)) == -1) {
         var->return_value = 1;
@@ -56,7 +48,7 @@ void env_redirection(char **str, var_t *var)
             var->return_value = 1;
             exit(1);
         }
-    } redirection_input(str, var, overwrite);
+    } redirection_outpout(str, var, overwrite);
 }
 
 void handle_outpout_redirection(char **str, var_t *var)
@@ -65,7 +57,7 @@ void handle_outpout_redirection(char **str, var_t *var)
     var->indice = get_indice_outpout(str);
     if (var->indice > 0) {
         check_error_outpout_redirection(str, var, &overwrite);
-        redirection_input(str, var, overwrite);
+        redirection_outpout(str, var, overwrite);
     }
 }
 
@@ -89,6 +81,6 @@ void handle_input_redirection(char **str, var_t *var)
             var->return_value = 1; exit(1);
         }
         str[var->indice] = NULL;
-        redirection_outpout(str, var);
+        redirection_input(str, var);
     }
 }
