9081396cbf74df0b33704e3088e7f01ce9581fec
Théo Fabiano <theo.fabiano@epitech.eu>
2023-03-17 09:58:17 +0100

    🚑 Critical hotfix

--- src/pipe.c
+++ src/pipe.c
@@ -9,7 +9,7 @@
 
 void begin_with_pipe(char **str, var_t *var)
 {
-    if (!my_strcmp(str[0], "|")) {
+    if (str[0] && !my_strcmp(str[0], "|")) {
         if (!var->pid) {
             write(2, "Invalid null command.\n", 22);
             var->return_value = 1;
