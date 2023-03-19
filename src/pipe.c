41392b58fbe29dd3076cf91fbdc785f65f581d7f
Théo Fabiano <theo.fabiano@epitech.eu>
2023-03-19 18:32:00 +0100

    🥅 Catch Invalid null command

--- src/pipe.c
+++ src/pipe.c
@@ -76,7 +76,7 @@ bool handle_pipe(char **str, var_t *var)
     var->pipedes = malloc(sizeof(int) * 2);
     var->indice = get_indice_pipe(str);
     if (var->indice > 0) {
-        if (!str[var->indice + 1]) {
+        if (!str[var->indice + 1] || !my_strcmp(str[var->indice + 1], "|")) {
             write(2, "Invalid null command.\n", 22);
             exit(EXIT_FAILURE);
         }
